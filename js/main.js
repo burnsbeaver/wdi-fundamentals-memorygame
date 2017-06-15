ea6f19077b79232a725e1f23249d2ae3b353d6cd
Burns Beaver <burnsbeaver@Burnss-MacBook-Pro.local>
2017-06-15 15:29:42 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);	
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+	alert("Sorry, try again");
+	}
+}
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
